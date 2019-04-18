d5aa440523357381cd77649fd2c565a59e354cca
CesarRivasP <cesarjose58@gmail.com>
2019-04-17 21:07:44 -0400

    Change url of calls

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,35 +4,35 @@ const { verifyToken } = require('../middlewares/authentication');
 
 const app = express();
 
-const Product = require('../models/products');
+const Product = require('../models/product');
 
 // -- Obtener productos
-app.get('/products', verifyToken, (request, response) => {
+app.get('/product', verifyToken, (request, response) => {
   // Traer todos los productos
   // populate debe cargar los usuarios y la categoria
   // paginado
 })
 
 // -- Obtener productos por id
-app.get('/products/:id', verifyToken, (request, response) => {
+app.get('/product/:id', verifyToken, (request, response) => {
   // Traer todos los productos
   // populate debe cargar los usuarios y la categoria
 })
 
 // -- Crear un nuevo producto
-app.post('/products', verifyToken, (request, response) => {
+app.post('/product', verifyToken, (request, response) => {
   // grabar el usuario
   // grabar una categoria del listado
 })
 
 // -- Actualizar un producto
-app.put('/products/:id', verifyToken, (request, response) => {
+app.put('/product/:id', verifyToken, (request, response) => {
   // Traer todos los productos
   // populate debe cargar los usuarios y la categoria
   // paginado
 })
 
 // -- Borrar un producto
-app.delete('/products/:id', verifyToken, (request, response) => {
+app.delete('/product/:id', verifyToken, (request, response) => {
   // Pasar disponible a falso
 })
