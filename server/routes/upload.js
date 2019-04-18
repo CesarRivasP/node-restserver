5a1d2c6b0420582421ea4e2f486b772244c0eeae
CesarRivasP <cesarjose58@gmail.com>
2019-04-18 18:28:12 -0400

    Added validated types and change for dynamic name

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -3,10 +3,21 @@ const fileUpload = require('express-fileupload');
 
 const app = express();
 
+
+const User = require('../models/user');
+const Category = require('../models/category');
+
+
 app.use( fileUpload({ useTempFiles: true }) );
 // al usar la funcion fileUpload, hace que todos los archivos que se carguen caigan dentro de 'request.files'
 
-app.put('/upload', (request, response) => {
+// app.put('/upload', (request, response) => {
+app.put('/upload/:type/:id', (request, response) => {
+  // After
+  let type = request.params.type;
+  let id = request.params.id;
+
+
   //Si no hay archivos
   if(!request.files){
     return response.status(400).json({
@@ -16,6 +27,20 @@ app.put('/upload', (request, response) => {
       }
     })
   }
+
+  // Validar tipo
+  let validatedTypes = ['users', 'products'];
+
+  if(validatedTypes.indexOf(type) < 0){
+    return response.status(400).json({
+      ok: false,
+      error: {
+        message: 'Los tipos permitidos son ' + validatedTypes.join(', '),
+        types: 'Tipo recibido: ' + type
+      }
+    })
+  }
+
 
   let archive = request.files.archive;  //archivo fisico
 
@@ -39,13 +64,20 @@ app.put('/upload', (request, response) => {
     })
   }
 
-  archive.mv(`uploads/${archive.name}`, (error) => {
+  // Cambiar nombre al archivo, este debe ser unico
+  // Tambien debe variar su nombre para prevenir el cache del navegador
+  let archiveName = `${id}-${ new Date().getMilliseconds() }.${extension}`
+
+  // archive.mv(`uploads//${type}/${archive.name}`, (error) => {  //Para accder al nombre del archivo
+  archive.mv(`uploads/${type}/${archiveName}`, (error) => {
     if(error) {
       return response.status(500).json({
         ok: false,
         error
       })
     }
+
+    // Aqui la imagen se cargo, esta en el filesystem
 
     response.json({
       ok: true,
