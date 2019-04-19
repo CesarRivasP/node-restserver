ec093d0fca7571f81b7dbea8c22a18ac62c34194
CesarRivasP <cesarjose58@gmail.com>
2019-04-19 17:52:49 -0400

    Create images route

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { verifyToken }= require('../middlewares/authentication');
 
 const app = express();
 
 // Ruta para desplegar inforomacion
-app.get('/image/:type/:img', (request, response) => {
+app.get('/images/:type/:img', verifyToken, (request, response) => {
   let type = request.params.type;
   let img = request.params.img;
 
-  let pathImg = `./uploads/${type}/${img}`;
-  let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+  // console.log(type);
+  console.log(img);
+  // let pathImg = `./uploads/${type}/${img}`;
 
-  // Si hay un type invalido o el tipo no existe, o se quiere hacer un servicio que retorne una img por defecto
-  response.sendFile(noImagePath)
+  let pathImage = path.resolve(__dirname, `../../uploads/${type}/${img}`);
+  console.log(pathImage);
+
+  if(fs.existsSync(pathImage)){
+    response.sendFile(pathImage)
+  }
+  else{
+    // Si hay un type invalido o el tipo no existe, o se quiere hacer un servicio que retorne una img por defecto
+    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+    response.sendFile(noImagePath)
+  }
+
 })
 
 // sendfile lee el content type del archivo y eso es lo que regresa, sea una imagen, json o html
